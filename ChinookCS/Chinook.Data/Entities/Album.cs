28f9ca70c52841f66ebb9bfae8fbf316711e6d94
Gregory General <gregoryqgeneral@gmail.com>
2018-09-10 20:38:37 -0600

    ClassExampleDOne9/10

---
+++ ChinookCS/Chinook.Data/Entities/Album.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data.Entities
+{
+    public class Album
+    {
+        [Table("Albums"]
+        [Key] public int AlbumID { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(160, ErrorMessage = "Title is limited to 120 char")]
+        public string Title { get; set; }
+        public int ArtistID { get; set; }
+        public int ReleaseYear { get; set; }
+        [StringLength(50, ErrorMessage = "Release Label is limited to 50 char")]
+        public string ReleaseLabel { get; set; }
+
+    }
+}

b220eed1562df85ded9018b696dc2084fc3601a0
GregoryGeneral <43020335+GregoryGeneral@users.noreply.github.com>
2018-09-21 20:25:39 -0600

    Reverse Engineering / Entities

--- ChinookCS/Chinook.Data/Entities/Album.cs
+++ ChinookCS/Chinook.Data/Entities/Album.cs
@@ -1,30 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
 
 namespace Chinook.Data.Entities
 {
-    [Table("Albums")]
     public class Album
     {
-        
-        [Key] public int AlbumID { get; set; }
-        [Required(ErrorMessage = "Title is required")]
-        [StringLength(160, ErrorMessage = "Title is limited to 120 char")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Album()
+        {
+            Tracks = new HashSet<Track>();
+        }
+
+        public int AlbumId { get; set; }
+
+        [Required(ErrorMessage ="Title is a required field")]
+        [StringLength(160,ErrorMessage="Title is limited to 160 characters.")]
         public string Title { get; set; }
-        public int ArtistID { get; set; }
+
+        public int ArtistId { get; set; }
+
         public int ReleaseYear { get; set; }
-        [StringLength(50, ErrorMessage = "Release Label is limited to 50 char")]
+
+        [StringLength(50)]
         public string ReleaseLabel { get; set; }
 
-
-        //navigational properties
         public virtual Artist Artist { get; set; }
-        //public virtual ICollection<Track> Tracks { get; set; }
 
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Track> Tracks { get; set; }
     }
 }
